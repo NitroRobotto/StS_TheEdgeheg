0653122f9b326f7c1a8e9e13dceb731ce1e70ca6
NitroRobotto <eliasmaidanik@gmail.com>
2020-07-11 16:58:03 +0200

    Add "Chaos Grenade" card

--- theDefault/src/main/java/theEdgeheg/actions/ChaosGrenadeAction.java
+++ theDefault/src/main/java/theEdgeheg/actions/ChaosGrenadeAction.java
@@ -1,6 +1,8 @@
 package theEdgeheg.actions;
 
+import com.evacipated.cardcrawl.mod.stslib.powers.StunMonsterPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -8,30 +10,32 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 /**
- * Deals damage. If it kills the target, heals the player.
+ * Deals damage. If it kills the target, all enemies are stunned.
  *  @author NITRO
  *  @version 1.0
  *  @since 2020-07-11
  */
-public class BloodyKatanaAction extends AbstractGameAction {
-    private int heal;
+public class ChaosGrenadeAction extends AbstractGameAction {
     private DamageInfo info;
 
-    public BloodyKatanaAction(AbstractCreature target, DamageInfo info, int healAmount) {
+    public ChaosGrenadeAction(AbstractCreature target, DamageInfo info) {
         this.info = info;
         this.setValues(target, info);
 
-        this.heal = healAmount;
         this.actionType = ActionType.DAMAGE;
         this.duration = 0.1F;
     }
 
     public void update() {
         if (this.duration == 0.1F && this.target != null) {
-            AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.SLASH_VERTICAL));
+            AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.FIRE));
             this.target.damage(this.info);
-            if ((((AbstractMonster)this.target).isDying || this.target.currentHealth <= 0) && !this.target.halfDead && !this.target.hasPower("Minion")) {
-                AbstractDungeon.player.heal(this.heal);
+            if ((((AbstractMonster)this.target).isDying || this.target.currentHealth <= 0) && !this.target.halfDead) {
+                AbstractCreature source = AbstractDungeon.player;
+                for (final AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                    AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, source,
+                            new StunMonsterPower(mo, 1)));
+                }
             }
 
             if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
--- theDefault/src/main/java/theEdgeheg/cards/attacks/ChaosGrenade.java
+++ theDefault/src/main/java/theEdgeheg/cards/attacks/ChaosGrenade.java
@@ -1,23 +1,28 @@
 package theEdgeheg.cards.attacks;
 
 import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.evacipated.cardcrawl.mod.stslib.powers.StunMonsterPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.EnergizedPower;
 import theEdgeheg.DefaultMod;
+import theEdgeheg.actions.ChaosGrenadeAction;
 import theEdgeheg.cards.AbstractDynamicCard;
 import theEdgeheg.cards.EdgehegCardTags;
 import theEdgeheg.characters.TheEdgeheg;
 import theEdgeheg.modifiers.DamageGunScalingModifier;
-import theEdgeheg.modifiers.PreciseModifier;
 
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static theEdgeheg.DefaultMod.makeCardPath;
 
-public class RegularGun extends AbstractDynamicCard {
+/**
+ * (0): Deal 5 Damage. If Fatal, stun all enemies. Damage scales with "GUNS". Upgrade: Stun target.
+ *  @author NITRO
+ *  @version 1.0
+ *  @since 2020-07-11
+ */
+public class ChaosGrenade extends AbstractDynamicCard {
 
     /*
      * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
@@ -25,7 +30,7 @@ public class RegularGun extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = DefaultMod.makeID(RegularGun.class.getSimpleName());
+    public static final String ID = DefaultMod.makeID(ChaosGrenade.class.getSimpleName());
     public static final String IMG = makeCardPath("shadow.jpg");
 
     // /TEXT DECLARATION/
@@ -37,29 +42,27 @@ public class RegularGun extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheEdgeheg.Enums.COLOR_PURPLE;
 
-    private static final int COST = 2;
-    private static final int DAMAGE = 13;
-    private static final int UPGRADE_PLUS_DMG = 4;
+    private static final int COST = 0;
+    private static final int DAMAGE = 5;
 
     // /STAT DECLARATION/
 
-    public RegularGun() {
+    public ChaosGrenade() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
 
         damage = baseDamage = DAMAGE;
         tags.add(EdgehegCardTags.GUN);
+        this.exhaust = true;
 
         CardModifierManager.addModifier(this, new DamageGunScalingModifier( true));
-        CardModifierManager.addModifier(this, new PreciseModifier( true));
         // We don't call "initializeDescription" here because addModifier already does it
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
-                AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-        addToBot(new ApplyPowerAction(p,p,new EnergizedPower(p,1)));
+        addToBot(new ChaosGrenadeAction(m, new DamageInfo(p, damage, damageTypeForTurn)));
+        if (upgraded) addToBot(new ApplyPowerAction(m, p, new StunMonsterPower(m)));
     }
 
     // Upgraded stats.
@@ -67,7 +70,7 @@ public class RegularGun extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            rawDescription = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
